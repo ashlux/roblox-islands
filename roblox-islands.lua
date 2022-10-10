f41203c59179abace6afd5eb6fff43a318cb5524
aratonstilts <114688812+aratonstilts@users.noreply.github.com>
2022-10-10 18:35:04 -0400

    Update roblox-islands.lua

--- roblox-islands.lua
+++ roblox-islands.lua
@@ -7228,7 +7228,7 @@ Item79.MouseButton1Click:Connect(function()
                     }
                     }
                     }
-                    game:GetService("ReplicatedStorage").rbxts_include.node_modules.net.out._NetManaged:FindFirstChild("cMgolqb/zbnjFj"):FireServer(unpack(args))
+                    game:GetService("ReplicatedStorage").rbxts_include.node_modules.net.out._NetManaged:FindFirstChild("hvyqwbKPmzDjwr/ezj")
                     wait()
                 end
             end

4b8d10528a1b1c588910c70312a8401f1317db0d
Ash Lux <ashlux@gmail.com>
2022-12-04 00:02:03 -0600

    Create string-utils.lua

---
+++ modules/string-utils.lua
@@ -0,0 +1,17 @@
+local function intToString(amount)
+    if (not amount) then
+      return "NaN"
+    end
+  
+    while true do
+        amount, k = string.gsub(amount, "^(-?%d+)(%d%d%d)", '%1,%2')
+        if (k==0) then
+            break
+        end
+    end
+    return amount
+end
+
+return {
+  intToString = intToString
+}

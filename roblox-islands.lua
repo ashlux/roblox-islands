a4e0f210fdfcb0ca44ff98e0f791df32bd2def82
aratonstilts <114688812+aratonstilts@users.noreply.github.com>
2022-12-05 10:26:21 -0500

    Update roblox-islands.lua

--- roblox-islands.lua
+++ roblox-islands.lua
@@ -16,7 +16,7 @@ print("Loading Complete!")
 
 local StarterGui = game:GetService("StarterGui")
 --placeId are in this order.  Home, OnlineIsland, Hub, Wilderness Islands
-if (game.PlaceId == 4872321990 and game.PlaceVersion ~= 996) or (game.PlaceId == 5626342417 and game.PlaceVersion ~= 649) or (game.PlaceId == 5899156129 and game.PlaceVersion ~= 574) or (game.PlaceId == 9501318975 and game.PlaceVersion ~= 217) then
+if (game.PlaceId == 4872321990 and game.PlaceVersion ~= 996) or (game.PlaceId == 5626342417 and game.PlaceVersion ~= 649) or (game.PlaceId == 5899156129 and game.PlaceVersion ~= 573) or (game.PlaceId == 9501318975 and game.PlaceVersion ~= 217) then
     updates = "😲 GUI might not function in this server 😲"
     StarterGui:SetCore("ChatMakeSystemMessage", {Color = Color3.fromRGB(250,0,0), Font = Enum.Font.SourceSansBold, TextSize = 18, Text = updates})
 end

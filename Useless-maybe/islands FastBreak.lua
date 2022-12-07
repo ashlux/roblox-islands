9a4af881bac47a212bc2b30872a8efbcbd513afd
aratonstilts <114688812+aratonstilts@users.noreply.github.com>
2022-12-07 11:10:20 -0500

    fastbreak updated

--- Useless-maybe/islands FastBreak.lua
+++ Useless-maybe/islands FastBreak.lua
@@ -5,22 +5,26 @@ local uis = game:GetService("UserInputService")
 local SMACKING = false
 
 local function onInputBegan(input, gp)
-    if gp then wait() end
+    
+    if gp then return end -- if clicking chat
+    
     if input.UserInputType == Enum.UserInputType.MouseButton1 then
         SMACKING = true
         while SMACKING do
-            if mouse.Target == nil then wait() else
-            local args = {
-            [1] = {
-            ["player_tracking_category"] = "join_from_web",
-            ["part"] = mouse.Target,
-            ["block"] = mouse.Target,
-            ["norm"] = mouse.Target:FindFirstChild("Position"),
-            ["pos"] = mouse.Target:FindFirstChild("Position")
-            }
-            }
-            game:GetService("ReplicatedStorage").rbxts_include.node_modules.net.out._NetManaged.CLIENT_BLOCK_HIT_REQUEST:InvokeServer(unpack(args))
-            wait()
+            task.wait()
+            if mouse.Target ~= nil then
+                pcall(function()
+                local args = {
+                [1] = {
+                ["player_tracking_category"] = "join_from_web",
+                ["part"] = mouse.Target,
+                ["block"] = mouse.Target,
+                ["norm"] = mouse.Target:FindFirstChild("Position"),
+                ["pos"] = mouse.Target:FindFirstChild("Position")
+                }
+                }
+                game:GetService("ReplicatedStorage").rbxts_include.node_modules.net.out._NetManaged.CLIENT_BLOCK_HIT_REQUEST:InvokeServer(unpack(args))
+                end)
             end
         end
     end

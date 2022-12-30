9273e4fa6faedb73da6dfb295153a810f7775992
Ash Lux <ashlux@gmail.com>
2022-12-30 11:23:08 -0600

    Create shop-prices-to-csv.lua

---
+++ Useless-maybe/shop-prices-to-csv.lua
@@ -0,0 +1,37 @@
+Island = game.Workspace.Islands:GetChildren()[1]
+
+local dt = os.date("*t", os.time())
+
+local joinCodes = ""
+for k,player in pairs(game.Players:GetChildren()) do
+	for _, ownerUserId in pairs(Island.Owners:GetChildren()) do
+		if (player.UserId == ownerUserId.Value) then
+			joinCodes = joinCodes .. " " .. player.JoinCode.Value
+		end
+	end
+end
+--print(joinCodes)
+
+
+local filename = dt.year .. "-" .. dt.month .. "-" .. dt.day .. ".csv"
+print(filename)
+if (not isfile(filename)) then
+	writefile(filename, "")
+end
+
+
+for k,vm in pairs(Island.Blocks:GetChildren()) do
+	if (vm.Name:find("vendingMachine") and vm.SellingContents and #vm.SellingContents:GetChildren() == 1) then	
+		local item = vm.SellingContents:GetChildren()[1]		
+		local amount = item.Amount.Value
+		local price = vm.TransactionPrice.Value
+		local mode = vm.Mode.Value
+		if mode == 0 then
+			--print("SELLING " .. amount .. " " .. item.Name .. " for " .. price)
+			appendfile(filename, "sell," .. joinCodes .. "," .. amount	 .. "," .. item.Name .. "," .. price .. "\n")
+		elseif mode == 1 then
+			--print("BUYING " .. item.Name .. " for " .. price)
+			appendfile(filename, "buy," .. joinCodes .. "," .. amount .. "," .. item.Name .. "," .. price .. "\n")
+		end
+	end
+end

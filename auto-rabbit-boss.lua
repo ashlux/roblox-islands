26410cdc9e33563af67f9b3ed51b4f2297e08fe8
aratonstilts <114688812+aratonstilts@users.noreply.github.com>
2023-01-23 02:07:20 -0500

    re

--- auto-rabbit-boss.lua
+++ auto-rabbit-boss.lua
@@ -29,7 +29,7 @@ end
 
 if keepGoing == false then return end
 
-local hitMobEvent =  game:GetService("ReplicatedStorage").rbxts_include.node_modules:FindFirstChild("@rbxts").net.out._NetManaged:FindFirstChild("imkd/yxapvyva")
+local hitMobEvent =  game:GetService("ReplicatedStorage").rbxts_include.node_modules:FindFirstChild("@rbxts").net.out._NetManaged:FindFirstChild("karwXNsygcznhp/OkAcbbwiJ")
 
 local function equipBane()
     local Bane = Player.Backpack:FindFirstChild("serpentsBane")

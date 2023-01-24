6899451ae13391033ec5e754dfa9adef05249bde
aratonstilts <114688812+aratonstilts@users.noreply.github.com>
2023-01-24 13:17:07 -0500

    Update lunar-area.lua

--- lunar-area.lua
+++ lunar-area.lua
@@ -139,6 +139,7 @@ local function createMainBackground()
     God.Text = "Break Rabbit"
     God.TextColor3 = Color3.fromRGB(200,200,150)
     God.TextScaled = true
+	God.Visible = false
     God.MouseButton1Click:Connect(function()
         Character.IsPlayer:Destroy()
         God.Text = "Rabbit should stop"

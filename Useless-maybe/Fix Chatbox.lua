ad8215616dba5db54859bede0f7a9c597cae36ff
aratonstilts <114688812+aratonstilts@users.noreply.github.com>
2023-01-24 14:23:24 -0500

    Update Fix Chatbox.lua

--- Useless-maybe/Fix Chatbox.lua
+++ Useless-maybe/Fix Chatbox.lua
@@ -1,1 +1,1 @@
-game:GetService("Players").MattOwnsTheWorld.PlayerGui.Chat.Frame.Position = UDim2.new(0, 87, 0, 0)
+game:GetService("Players").LocalPlayer.PlayerGui.Chat.Frame.Position = UDim2.new(0, 0, 1, -335)

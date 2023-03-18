8dcd89993efc868276e2aa9bdb65f7a756090564
aratonstilts <114688812+aratonstilts@users.noreply.github.com>
2023-03-18 14:12:19 -0400

    Update Fix Chatbox.lua

--- Useless-maybe/Fix Chatbox.lua
+++ Useless-maybe/Fix Chatbox.lua
@@ -1,1 +1,3 @@
-game:GetService("Players").LocalPlayer.PlayerGui.Chat.Frame.Position = UDim2.new(0, 0, 1, -335)
+local chatFrame = game:GetService("Players").LocalPlayer.PlayerGui.Chat.Frame
+chatFrame.Position = UDim2.new(0, 0, 1, 0)
+chatFrame.AnchorPoint = Vector2.new(0,1)

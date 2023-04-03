19116bcc2f853e4755d9b69b2c1940c31dc5f5d2
aratonstilts <114688812+aratonstilts@users.noreply.github.com>
2023-04-03 05:12:38 -0400

    Update remoteEvents.lua

--- remoteEvents.lua
+++ remoteEvents.lua
@@ -1,11 +1,11 @@
 local StarterGui = game:GetService("StarterGui")
 
 --placeId are in this order.  Home, OnlineIsland, Hub, Wilderness Islands
-if (game.PlaceId == 4872321990 and game.PlaceVersion ~= 1077) or (game.PlaceId == 5626342417 and game.PlaceVersion ~= 727) or (game.PlaceId == 5899156129 and game.PlaceVersion ~= 651) or (game.PlaceId == 9501318975 and game.PlaceVersion ~= 295) then
+if (game.PlaceId == 4872321990 and game.PlaceVersion ~= 1078) or (game.PlaceId == 5626342417 and game.PlaceVersion ~= 728) or (game.PlaceId == 5899156129 and game.PlaceVersion ~= 652) or (game.PlaceId == 9501318975 and game.PlaceVersion ~= 296) then
     updates = "😲 GUI might not function in this server 😲"
     StarterGui:SetCore("ChatMakeSystemMessage", {Color = Color3.fromRGB(250,0,0), Font = Enum.Font.SourceSansBold, TextSize = 18, Text = updates})
 else
-    updates = "[Matt]: Updated and tested 4/2."
+    updates = "[Matt]: Updated and tested 4/3."
     StarterGui:SetCore("ChatMakeSystemMessage", {Color = Color3.fromRGB(0,255,255), Font = Enum.Font.SourceSansBold, TextSize = 18, Text = updates})
 end
 

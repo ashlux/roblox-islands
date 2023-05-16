f8ac1b470a82e4fe21bbfdd6f058ac73272c650c
aratonstilts <114688812+aratonstilts@users.noreply.github.com>
2023-05-16 10:57:40 -0400

    Update remoteEvents.lua

--- remoteEvents.lua
+++ remoteEvents.lua
@@ -1,5 +1,14 @@
 local StarterGui = game:GetService("StarterGui")
 
+local function AntiAFK() -- keeps you from going afk by clicking corner of screen when player goes "Idled"
+    game:GetService('Players').LocalPlayer.Idled:Connect(function()
+    game:GetService('VirtualUser'):CaptureController();
+    wait();
+    game:GetService('VirtualUser'):ClickButton2(Vector2.new(0,0))
+    end)
+end
+task.spawn(AntiAFK)
+
 --placeId are in this order.  Home, OnlineIsland, Hub, Wilderness Islands
 if (game.PlaceId == 4872321990 and game.PlaceVersion ~= 1085) or (game.PlaceId == 5626342417 and game.PlaceVersion ~= 735) or (game.PlaceId == 5899156129 and game.PlaceVersion ~= 659) or (game.PlaceId == 9501318975 and game.PlaceVersion ~= 303) then
     updates = "😲 Be careful and make sure to test stuff on alts first!! Im no longer activly checking everything on the script. 😲"

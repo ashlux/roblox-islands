7d0f915e4329a02f08f859eb917ba269810c3756
aratonstilts <114688812+aratonstilts@users.noreply.github.com>
2023-09-01 00:39:30 -0400

    Update remoteEvents.lua

--- remoteEvents.lua
+++ remoteEvents.lua
@@ -14,17 +14,17 @@ task.spawn(AntiAFK)
 sendTrade = game:GetService("ReplicatedStorage"):WaitForChild("rbxts_include"):WaitForChild("node_modules"):WaitForChild("@rbxts"):WaitForChild("net"):WaitForChild("out"):WaitForChild("_NetManaged"):FindFirstChild("vgizdqpvely/pgaahtkhrfaswffkrliV")
 addTrade = game:GetService("ReplicatedStorage"):WaitForChild("rbxts_include"):WaitForChild("node_modules"):WaitForChild("@rbxts"):WaitForChild("net"):WaitForChild("out"):WaitForChild("_NetManaged"):FindFirstChild("vgizdqpvely/ybDhaheMnowgzfjn")
 tradeAccept = game:GetService("ReplicatedStorage"):WaitForChild("rbxts_include"):WaitForChild("node_modules"):WaitForChild("@rbxts"):WaitForChild("net"):WaitForChild("out"):WaitForChild("_NetManaged"):FindFirstChild("vgizdqpvely/Npcln")
-openVending = game:GetService("ReplicatedStorage"):WaitForChild("rbxts_include"):WaitForChild("node_modules"):WaitForChild("@rbxts"):WaitForChild("net"):WaitForChild("out"):WaitForChild("_NetManaged"):FindFirstChild("pkluqnvptVsxBtegpejBYiNywkkxi/xWlleubphz")
-emptyItem = game:GetService("ReplicatedStorage"):WaitForChild("rbxts_include"):WaitForChild("node_modules"):WaitForChild("@rbxts"):WaitForChild("net"):WaitForChild("out"):WaitForChild("_NetManaged"):FindFirstChild("pkluqnvptVsxBtegpejBYiNywkkxi/qHfkufgtgxnduntypswgupeyjcadzqxqoyas")
+openVending = game:GetService("ReplicatedStorage"):WaitForChild("rbxts_include"):WaitForChild("node_modules"):WaitForChild("@rbxts"):WaitForChild("net"):WaitForChild("out"):WaitForChild("_NetManaged"):WaitForChild("HPuzpamlOoiXwjAdzgSmWitamew/cxzqfdq")
+emptyItem = game:GetService("ReplicatedStorage"):WaitForChild("rbxts_include"):WaitForChild("node_modules"):WaitForChild("@rbxts"):WaitForChild("net"):WaitForChild("out"):WaitForChild("_NetManaged"):WaitForChild("HPuzpamlOoiXwjAdzgSmWitamew/myBgzpwukcejNafa")
 refillItem = emptyItem
-refillCoins = game:GetService("ReplicatedStorage"):WaitForChild("rbxts_include"):WaitForChild("node_modules"):WaitForChild("@rbxts"):WaitForChild("net"):WaitForChild("out"):WaitForChild("_NetManaged"):FindFirstChild("pkluqnvptVsxBtegpejBYiNywkkxi/TWhzuqFkxvcwfreyaioiJqfaholnlvlkpnosnAtU")
-takeCoins = game:GetService("ReplicatedStorage"):WaitForChild("rbxts_include"):WaitForChild("node_modules"):WaitForChild("@rbxts"):WaitForChild("net"):WaitForChild("out"):WaitForChild("_NetManaged"):FindFirstChild("pkluqnvptVsxBtegpejBYiNywkkxi/jHufcfjX")
-changeSettings = game:GetService("ReplicatedStorage"):WaitForChild("rbxts_include"):WaitForChild("node_modules"):WaitForChild("@rbxts"):WaitForChild("net"):WaitForChild("out"):WaitForChild("_NetManaged"):FindFirstChild("pkluqnvptVsxBtegpejBYiNywkkxi/oigriLrbDIlhrktwfqXbqxuubptvxusjiul")
-closeVending = game:GetService("ReplicatedStorage"):WaitForChild("rbxts_include"):WaitForChild("node_modules"):WaitForChild("@rbxts"):WaitForChild("net"):WaitForChild("out"):WaitForChild("_NetManaged"):FindFirstChild("pkluqnvptVsxBtegpejBYiNywkkxi/kcfPknrkEGcsgenobqcOnmyLlneyb")
+refillCoins = game:GetService("ReplicatedStorage"):WaitForChild("rbxts_include"):WaitForChild("node_modules"):WaitForChild("@rbxts"):WaitForChild("net"):WaitForChild("out"):WaitForChild("_NetManaged"):WaitForChild("HPuzpamlOoiXwjAdzgSmWitamew/gcbtzYoyxrphkzcvsgupsXTrtors")
+takeCoins = game:GetService("ReplicatedStorage"):WaitForChild("rbxts_include"):WaitForChild("node_modules"):WaitForChild("@rbxts"):WaitForChild("net"):WaitForChild("out"):WaitForChild("_NetManaged"):WaitForChild("HPuzpamlOoiXwjAdzgSmWitamew/HphUazcwzraCbrpdhqgCzYxKqrprj")
+changeSettings = game:GetService("ReplicatedStorage"):WaitForChild("rbxts_include"):WaitForChild("node_modules"):WaitForChild("@rbxts"):WaitForChild("net"):WaitForChild("out"):WaitForChild("_NetManaged"):WaitForChild("HPuzpamlOoiXwjAdzgSmWitamew/qdqAyxrVToyhe")
+closeVending = game:GetService("ReplicatedStorage"):WaitForChild("rbxts_include"):WaitForChild("node_modules"):WaitForChild("@rbxts"):WaitForChild("net"):WaitForChild("out"):WaitForChild("_NetManaged"):WaitForChild("HPuzpamlOoiXwjAdzgSmWitamew/ysxecesOkdltitbdajugdqsljAmwzucpmuw")
 useNet = game:GetService("ReplicatedStorage").rbxts_include.node_modules["@rbxts"].net.out["_NetManaged"] -- sorry im too lazy to update this one
 
 if emptyItem then
-    updates = "⚠️ Test stuff on alts first!  vendings and chests and sickles are fine right now though."
+    updates = "⚠️ Test stuff on alts first!  vendings are fine right now though."
     StarterGui:SetCore("ChatMakeSystemMessage", {Color = Color3.fromRGB(150,150,255), Font = Enum.Font.SourceSansBold, TextSize = 20, Text = updates})
 else
     updates = "⚠️ Test stuff on alts first!! Trading and vendings are broken!"
